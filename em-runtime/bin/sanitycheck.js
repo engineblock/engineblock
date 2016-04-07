b3452e8de758a3a704ae02b9f94e63a9c1a8c063
Jonathan Shook <jshook@gmail.com>
2016-04-06 20:46:22 -0500

    Clean up sanitycheck.js

--- em-runtime/bin/sanitycheck.js
+++ em-runtime/bin/sanitycheck.js
@@ -1,6 +1,4 @@
-//var sc = new com.metawiring.load.cycler.ScenarioController();
-
-sc.start('type=diag;alias=test;min=1;max=10000;threads=10;interval=2000;');
+sc.start('type=diag;alias=test;min=1;max=10000000;threads=10;interval=2000;');
 
 print('waiting 2000 ms');
 sc.waitMillis(2000);
