482aea538af605bdf3cd46c4dee8c7de7028f55a
Jonathan Shook <jshook@gmail.com>
2016-06-17 21:47:54 -0500

    engineblock-32 Allow the CLI to use param=value format for activities (#35)

--- eb-core/src/main/java/io/engineblock/cli/EBCLIOptions.java
+++ eb-core/src/main/java/io/engineblock/cli/EBCLIOptions.java
@@ -4,9 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.security.InvalidParameterException;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * No CLI parser lib is useful for command structures, it seems. So we have this instead, which is good enough.
@@ -14,15 +13,22 @@ import java.util.List;
  */
 public class EBCLIOptions {
 
-    private final static Logger logger = LoggerFactory.getLogger(EBCLIOptions.class);
-
     public static final String docoptFileName = "docopt.txt";
-
+    private final static Logger logger = LoggerFactory.getLogger(EBCLIOptions.class);
     private static final String ACTIVITY = "activity";
     private static final String VERSION = "version";
     private static final String SCRIPT = "script";
     private static final String ACTIVITY_TYPES = "activitytypes";
     private static final String HELP = "help";
+    private static final String METRICS_PREFIX = "metrics-prefix";
+    private static final String REPORT_GRAPHITE_TO = "report-graphite-to";
+    private static final Set<String> reserved_words = new HashSet<String>() {{
+        addAll(
+                Arrays.asList(
+                        ACTIVITY, VERSION, SCRIPT, ACTIVITY_TYPES, HELP, METRICS_PREFIX, REPORT_GRAPHITE_TO
+                )
+        );
+    }};
 
     private LinkedList<Cmd> cmdList = new LinkedList<>();
     private boolean wantsVersion = false;
@@ -30,6 +36,8 @@ public class EBCLIOptions {
     private String wantsActivityHelpFor;
     private boolean wantsActivityTypes = false;
     private boolean wantsBasicHelp = false;
+    private String reportGraphiteTo = "";
+    private String metricsPrefix = "engineblock.";
 
     EBCLIOptions(String[] args) {
         parse(args);
@@ -41,7 +49,7 @@ public class EBCLIOptions {
             addAll(Arrays.asList(args));
         }};
 
-        if (arglist.peekFirst()==null) {
+        if (arglist.peekFirst() == null) {
             wantsBasicHelp = true;
             return;
         }
@@ -50,8 +58,14 @@ public class EBCLIOptions {
             String word = arglist.removeFirst();
             switch (word) {
                 case ACTIVITY:
+                    List<String> activitydef = new ArrayList<String>();
+                    while (arglist.size()>0 && !reserved_words.contains(arglist.peekFirst()) && arglist.peekFirst().contains("=")) {
+                        activitydef.add(arglist.removeFirst());
+                    }
+                    cmdList.add(new Cmd(CmdType.activity, activitydef.stream().map(s -> s+";").collect(Collectors.joining())));
+                    break;
                 case SCRIPT:
-                    cmdList.add(new Cmd(CmdType.valueOf(word),arglist.removeFirst()));
+                    cmdList.add(new Cmd(CmdType.valueOf(word), arglist.removeFirst()));
                     break;
                 case VERSION:
                     wantsVersion = true;
@@ -59,13 +73,19 @@ public class EBCLIOptions {
                 case HELP:
                 case "-h":
                 case "--help":
-                    if (arglist.peekFirst()==null) {
+                    if (arglist.peekFirst() == null) {
                         wantsBasicHelp = true;
                         logger.info("getting basic help");
                     } else {
                         wantsActivityHelp = true;
                         wantsActivityHelpFor = arglist.removeFirst();
                     }
+                    break;
+                case REPORT_GRAPHITE_TO:
+                    reportGraphiteTo = arglist.removeFirst();
+                    break;
+                case METRICS_PREFIX:
+                    metricsPrefix = arglist.removeFirst();
                     break;
                 case ACTIVITY_TYPES:
                     wantsActivityTypes = true;
@@ -92,12 +112,21 @@ public class EBCLIOptions {
     public boolean wantsActivityHelp() {
         return wantsActivityHelp;
     }
+
     public String wantsActivityHelpFor() {
         return wantsActivityHelpFor;
     }
 
     public boolean wantsBasicHelp() {
         return wantsBasicHelp;
+    }
+
+    public String wantsReportGraphiteTo() {
+        return reportGraphiteTo;
+    }
+
+    public String wantsMetricsPrefix() {
+        return metricsPrefix;
     }
 
     public static enum CmdType {
@@ -108,12 +137,23 @@ public class EBCLIOptions {
     public static class Cmd {
         public CmdType cmdType;
         public String cmdSpec;
+
         public Cmd(CmdType cmdType, String cmdSpec) {
-            this.cmdSpec = cmdSpec; this.cmdType = cmdType;
+            this.cmdSpec = cmdSpec;
+            this.cmdType = cmdType;
         }
-        public String getCmdSpec() { return cmdSpec; }
-        public CmdType getCmdType() { return cmdType; }
-        public String toString() { return "type:" + cmdType + ";spec=" + cmdSpec; }
+
+        public String getCmdSpec() {
+            return cmdSpec;
+        }
+
+        public CmdType getCmdType() {
+            return cmdType;
+        }
+
+        public String toString() {
+            return "type:" + cmdType + ";spec=" + cmdSpec;
+        }
     }
 
 }
