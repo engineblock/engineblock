50d7c38554897259ecf6036514cb38ce9a6cb6d4
Guy Bolton King <guy.boltonking@datastax.com>
2017-10-18 11:23:24 +0100

    Added failing test for significant digits in HdrHistogram logs.

--- eb-api/src/test/java/io/engineblock/metrics/HistoIntervalLoggerTest.java
+++ eb-api/src/test/java/io/engineblock/metrics/HistoIntervalLoggerTest.java
@@ -18,6 +18,7 @@
 package io.engineblock.metrics;
 
 import org.HdrHistogram.EncodableHistogram;
+import org.HdrHistogram.Histogram;
 import org.HdrHistogram.HistogramLogReader;
 import org.HdrHistogram.Recorder;
 import org.testng.annotations.Test;
@@ -41,8 +42,10 @@ public class HistoIntervalLoggerTest {
 
         HistoIntervalLogger hil = new HistoIntervalLogger("loggertest", tempFile, Pattern.compile(".*"), 1000);
 
+        final int significantDigits = 4;
+
         NicerHistogram nicerHistogram = new NicerHistogram(
-                "histo1", new DeltaHdrHistogramReservoir("histo1", new Recorder(4)));
+                "histo1", new DeltaHdrHistogramReservoir("histo1", new Recorder(significantDigits)));
 
         hil.onHistogramAdded("histo1",nicerHistogram);
 
@@ -71,9 +74,10 @@ public class HistoIntervalLoggerTest {
             }
             histos.add(histogram);
         };
+
         assertThat(histos.size()).isEqualTo(2);
-
-
+        assertThat(histos.get(0)).isInstanceOf(Histogram.class);
+        assertThat(((Histogram)histos.get(0)).getNumberOfSignificantValueDigits()).isEqualTo(significantDigits);
     }
 
     private void delay(int i) {
