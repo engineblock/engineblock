6af2a0cd1c629cc9577a0d706d928e1d534d9168
Jonathan Shook <jshook@gmail.com>
2018-02-26 02:10:04 -0600

    diag can rate limit internally to test CO

--- at-basics/src/main/java/io/engineblock/activities/diag/DiagActivity.java
+++ at-basics/src/main/java/io/engineblock/activities/diag/DiagActivity.java
@@ -1,28 +1,33 @@
 /*
-*   Copyright 2016 jshook
-*   Licensed under the Apache License, Version 2.0 (the "License");
-*   you may not use this file except in compliance with the License.
-*   You may obtain a copy of the License at
-*
-*       http://www.apache.org/licenses/LICENSE-2.0
-*
-*   Unless required by applicable law or agreed to in writing, software
-*   distributed under the License is distributed on an "AS IS" BASIS,
-*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*   See the License for the specific language governing permissions and
-*   limitations under the License.
-*/
+ *   Copyright 2016 jshook
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package io.engineblock.activities.diag;
 
 import com.codahale.metrics.Histogram;
 import io.engineblock.activityapi.core.Activity;
+import io.engineblock.activityapi.core.ActivityDefObserver;
 import io.engineblock.activityimpl.ActivityDef;
 import io.engineblock.activityimpl.SimpleActivity;
 import io.engineblock.metrics.ActivityMetrics;
+import io.engineblock.rates.RateLimiter;
+import io.engineblock.rates.RateLimiters;
+import io.engineblock.rates.RateSpec;
 
-public class DiagActivity extends SimpleActivity implements Activity {
+public class DiagActivity extends SimpleActivity implements Activity, ActivityDefObserver {
 
     protected Histogram delayHistogram;
+    private RateLimiter diagRateLimiter;
 
     public DiagActivity(ActivityDef activityDef) {
         super(activityDef);
@@ -30,11 +35,25 @@ public class DiagActivity extends SimpleActivity implements Activity {
 
     @Override
     public void initActivity() {
-        delayHistogram = ActivityMetrics.histogram(activityDef,"delay");
+        delayHistogram = ActivityMetrics.histogram(activityDef, "delay");
         Integer initdelay = activityDef.getParams().getOptionalInteger("initdelay").orElse(0);
         try {
             Thread.sleep(initdelay);
         } catch (InterruptedException ignored) {
         }
+    }
+
+    public RateLimiter getDiagRateLimiter() {
+        return diagRateLimiter;
+    }
+
+    @Override
+    public synchronized void onActivityDefUpdate(ActivityDef activityDef) {
+        super.onActivityDefUpdate(activityDef);
+
+        activityDef.getParams()
+                .getOptionalString("diagrate")
+                .map(RateSpec::new)
+                .ifPresent(spec -> diagRateLimiter= RateLimiters.createOrUpdate(getActivityDef(),diagRateLimiter,spec));
     }
 }
