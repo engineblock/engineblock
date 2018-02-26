dbd12dc65b9a98f471fb89de0d0e749efdd18b13
Jonathan Shook <jshook@gmail.com>
2018-02-26 02:09:38 -0600

    activities now own rates and their creation

--- eb-api/src/main/java/io/engineblock/activityapi/core/Activity.java
+++ eb-api/src/main/java/io/engineblock/activityapi/core/Activity.java
@@ -18,17 +18,20 @@
 package io.engineblock.activityapi.core;
 
 import io.engineblock.activityapi.cyclelog.filters.IntPredicateDispenser;
-import io.engineblock.activityapi.output.OutputDispenser;
 import io.engineblock.activityapi.input.InputDispenser;
+import io.engineblock.activityapi.output.OutputDispenser;
 import io.engineblock.activityimpl.ActivityDef;
 import io.engineblock.activityimpl.ParameterMap;
 import io.engineblock.activityimpl.SimpleActivity;
+import io.engineblock.rates.RateLimiter;
+
+import java.util.function.Supplier;
 
 /**
  * Provides the components needed to build and run an activity a runtime.
  * The easiest way to build a useful Activity is to extend {@link SimpleActivity}.
  */
-public interface Activity extends Comparable<Activity> {
+public interface Activity extends Comparable<Activity>, ActivityDefObserver {
 
     /**
      * Register an object which should be closed after this activity is shutdown.
@@ -84,5 +87,80 @@ public interface Activity extends Comparable<Activity> {
     default String getCycleSummary() {
         return getActivityDef().getCycleSummary();
     }
+
+    /**
+     * Get the current cycle rate limiter for this activity.
+     * The cycle rate limiter is used to throttle the rate at which
+     * cycles are dispatched across all threads in the activity
+     * @return the cycle {@link RateLimiter}
+     */
+    RateLimiter getCycleLimiter();
+
+    /**
+     * Set the cycle rate limiter for this activity. This method should only
+     * be used in a non-concurrent context. Otherwise, the supplier version
+     * {@link #getCycleRateLimiter(Supplier)} should be used.
+     * @param rateLimiter The cycle {@link RateLimiter} for this activity
+     */
+    void setCycleLimiter(RateLimiter rateLimiter);
+
+    /**
+     * Get or create the cycle rate limiter in a safe way. Implementations
+     * should ensure that this method is synchronized or that each requestor
+     * gets the same cycle rate limiter for the activity.
+     * @param supplier A {@link RateLimiter} {@link Supplier}
+     * @return An extant or newly created cycle {@link RateLimiter}
+     */
+    RateLimiter getCycleRateLimiter(Supplier<? extends RateLimiter> supplier);
+
+    /**
+     * Get the current stride rate limiter for this activity.
+     * The stride rate limiter is used to throttle the rate at which
+     * new strides are dispatched across all threads in an activity.
+     * @return The stride {@link RateLimiter}
+     */
+    RateLimiter getStrideLimiter();
+
+    /**
+     * Set the stride rate limiter for this activity. This method should only
+     * be used in a non-concurrent context. Otherwise, the supplier version
+     * {@link #getStrideRateLimiter(Supplier)}} should be used.
+     * @param rateLimiter The stride {@link RateLimiter} for this activity.
+     */
+    void setStrideLimiter(RateLimiter rateLimiter);
+
+    /**
+     * Get or create the stride {@link RateLimiter} in a concurrent-safe
+     * way. Implementations should ensure that this method is synchronized or
+     * that each requestor gets the same stride rate limiter for the activity.
+     * @param supplier A {@link RateLimiter} {@link Supplier}
+     * @return An extant or newly created stride {@link RateLimiter}
+     */
+    RateLimiter getStrideRateLimiter(Supplier<? extends RateLimiter> supplier);
+
+    /**
+     * Get the current phase rate limiter for this activity.
+     * The phase rate limiter is used to throttle the rate at which
+     * new phases are dispatched across all threads in an activity.
+     * @return The stride {@link RateLimiter}
+     */
+    RateLimiter getPhaseLimiter();
+
+    /**
+     * Set the phase rate limiter for this activity. This method should only
+     * be used in a non-concurrent context. Otherwise, the supplier version
+     * {@link #getPhaseRateLimiter(Supplier)}} should be used.
+     * @param rateLimiter The phase {@link RateLimiter} for this activity.
+     */
+    void setPhaseLimiter(RateLimiter rateLimiter);
+
+    /**
+     * Get or create the phase {@link RateLimiter} in a concurrent-safe
+     * way. Implementations should ensure that this method is synchronized or
+     * that each requestor gets the same phase rate limiter for the activity.
+     * @param supplier A {@link RateLimiter} {@link Supplier}
+     * @return An extant or newly created phase {@link RateLimiter}
+     */
+    RateLimiter getPhaseRateLimiter(Supplier<? extends RateLimiter> supplier);
 
 }
