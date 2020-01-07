887bb2c44410a1afaa7f1270a5436603699de465
Jonathan Shook <jshook@gmail.com>
2020-01-07 14:25:09 -0600

    fix for doc fixes

--- eb-docs/src/main/java/io/engineblock/docs/EngineblockMarkdownManifest.java
+++ eb-docs/src/main/java/io/engineblock/docs/EngineblockMarkdownManifest.java
@@ -1,14 +1,14 @@
 package io.engineblock.docs;
 
 import io.virtdata.annotations.Service;
-import io.virtdata.docsys.api.DocNameSpacesBinder;
 import io.virtdata.docsys.api.Docs;
+import io.virtdata.docsys.api.DocsBinder;
 import io.virtdata.docsys.api.DocsysDynamicManifest;
 
 @Service(DocsysDynamicManifest.class)
 public class EngineblockMarkdownManifest implements DocsysDynamicManifest {
     @Override
-    public DocNameSpacesBinder getDocs() {
+    public DocsBinder getDocs() {
         return new Docs().namespace("docs-for-eb")
                 .addFirstFoundPath("eb-cli/src/main/resources/docs-for-eb/",
                         "docs-for-eb/")
